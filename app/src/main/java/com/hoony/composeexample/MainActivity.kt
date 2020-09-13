3fe9ea8ddaf62b7fd0b34d5fb2b210c060bf2b4a
thkim9373 <thkim9373@gmail.com>
2020-09-13 23:50:45 +0900

    Save commit.

--- app/src/main/java/com/hoony/composeexample/MainActivity.kt
+++ app/src/main/java/com/hoony/composeexample/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.ui.tooling.preview.Preview
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        setContent {
+//            Greeting("Android")
+//        }
         setContent {
-            Greeting("Android")
+            NewStory()
         }
     }
 
@@ -24,5 +27,18 @@ class MainActivity : AppCompatActivity() {
     @Composable
     fun PreviewGreeting() {
         Greeting("Android")
+    }
+
+    @Composable
+    fun NewStory() {
+        Text("A day in Shark Fin Cove")
+        Text("Davenport, California")
+        Text("December 2018")
+    }
+
+    @Preview
+    @Composable
+    fun DefaultPreview() {
+        NewStory()
     }
 }
